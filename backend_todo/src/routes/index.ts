d7e45a111aa7d03ecd05f459a65ca1cf54e0f2bf
DubiumEkb <dubium-ekb@yandex.ru>
2023-10-07 23:04:33 +0500

    start

--- backend_todo/src/routes/index.ts
+++ backend_todo/src/routes/index.ts
@@ -1,1 +1,16 @@
-export const routes = (request:any, response: any) => {}
+export const routes = (request: any, response: any) => {
+	if (request.url === "/") {
+		if (request.method === "GET") {
+			response.end("Welcome!")
+		} else if (request.method === "POST") {
+		} else if (request.method === "PATCH") {
+		} else if (request.method === "DELETE") {
+		}
+	} else if (request.url === "/about") {
+		if (request.method === "GET") {
+			response.end("About!")
+		}
+	}
+
+	console.log(request.method, request.url)
+}
